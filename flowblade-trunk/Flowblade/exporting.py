c6ae7a881bb4649cbb877830b78582adacd03215
Janne Liljeblad <janne.liljeblad@gmail.com>
2012-12-09 21:46:51 +0200

    Add some new files

---
+++ flowblade-trunk/Flowblade/exporting.py
@@ -0,0 +1,48 @@
+"""
+    Flowblade Movie Editor is a nonlinear video editor.
+    Copyright 2012 Janne Liljeblad.
+
+    This file is part of Flowblade Movie Editor <http://code.google.com/p/flowblade>.
+
+    Flowblade Movie Editor is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Flowblade Movie Editor is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Flowblade Movie Editor.  If not, see <http://www.gnu.org/licenses/>.
+"""
+import xml.dom.minidom
+
+
+def DVD_AUTHOR_export(seq):
+    impl = xml.dom.minidom.getDOMImplementation()
+    doc = impl.createDocument(None, "dvdauthor", None)
+    
+    top_element = doc.documentElement
+    
+    wmgm_element = doc.createElement("wmgm")
+    top_element.appendChild(wmgm_element)
+    
+    titleset_element = doc.createElement("titleset")
+    top_element.appendChild(titleset_element)
+    
+    titles_element = doc.createElement("titles")
+    titleset_element.appendChild(titles_element)
+
+    pgc_element = doc.createElement("pgc")
+    titles_element.appendChild(pgc_element)
+
+    vob_element = doc.createElement("vob")
+    vob_element.setAttribute("file", "video1.mpg")
+    pgc_element.appendChild(vob_element)
+    
+    #f = open('/home/janne/dvdauthor.xml', 'wb')
+    #doc.writexml(f, encoding='utf-8')
+    #f.close()
+
