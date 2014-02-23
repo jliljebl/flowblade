da4d7cb57fc522f962bad7e7430bb3aec23b9e79
Janne Liljeblad <janne.liljeblad@gmail.com>
2014-02-24 01:02:38 +0200

    Make curves curve line wider

--- flowblade-trunk/Flowblade/extraeditors.py
+++ flowblade-trunk/Flowblade/extraeditors.py
@@ -1,4 +1,22 @@
+"""
+    Flowblade Movie Editor is a nonlinear video editor.
+    Copyright 2014 Janne Liljeblad.
 
+    This file is part of Flowblade Movie Editor <http://code.google.com/p/flowblade>.
+
+    Flowblade Movie Editor is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Flowblade Movie Editor is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Flowblade Movie Editor. If not, see <http://www.gnu.org/licenses/>.
+"""
 
 import gtk
 import math
@@ -575,6 +593,7 @@ class CurvesBoxEditor(BoxEditor):
         
         # curve
         cr.set_source_rgb(*self.curve_color)#  seg.setColor( CURVE_COLOR );
+        cr.set_line_width(1.5)
         cp = self.curve.get_curve(True) #we get 256 values
         px, py = BoxEditor.get_box_panel_point(self, 0, cp[0], 255)
         cr.move_to(px, py)
