80c6409b2d959f607c400e7b645a634a98f242b7
Janne Liljeblad <janne.liljeblad@gmail.com>
2016-10-09 23:40:50 +0300

    Middlebar testing

--- flowblade-trunk/Flowblade/middlebar.py
+++ flowblade-trunk/Flowblade/middlebar.py
@@ -159,9 +159,11 @@ def fill_with_TC_LEFT_pattern(buttons_row, window):
     buttons_row.pack_start(w.big_TC.widget, False, True, 0)
     buttons_row.pack_start(guiutils.get_pad_label(7, MIDDLE_ROW_HEIGHT), False, True, 0) #### NOTE!!!!!! THIS DETERMINES THE HEIGHT OF MIDDLE ROW
     buttons_row.pack_start(w.modes_selector.widget, False, True, 0)
-    buttons_row.pack_start(Gtk.Label(), True, True, 0)
     if editorstate.SCREEN_WIDTH > 1279:
+        buttons_row.pack_start(guiutils.get_pad_label(10, 10), False, True, 0)
         buttons_row.pack_start(_get_tools_buttons(), False, True, 0)
+        buttons_row.pack_start(Gtk.Label(), True, True, 0)
+    else:
         buttons_row.pack_start(Gtk.Label(), True, True, 0)
     buttons_row.pack_start(_get_undo_buttons_panel(), False, True, 0)
     buttons_row.pack_start(Gtk.Label(), True, True, 0)
