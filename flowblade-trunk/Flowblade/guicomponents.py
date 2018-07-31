7be7a0ad9e7315380928904675ae7358aa74923a
Janne Liljeblad <janne.liljeblad@gmail.com>
2018-07-31 20:34:19 +0300

    Fix MonitorTCDisplay to work with darker themes

--- flowblade-trunk/Flowblade/guicomponents.py
+++ flowblade-trunk/Flowblade/guicomponents.py
@@ -2233,9 +2233,12 @@ class MonitorTCDisplay:
 
         # Draw round rect with gradient and stroke around for thin bezel
         self._round_rect_path(cr)
-        cr.set_source_rgb(0.2, 0.2, 0.2)
+        if editorpersistance.prefs.theme == appconsts.LIGHT_THEME:
+            cr.set_source_rgb(0.2, 0.2, 0.2)
+        else:
+            cr.set_source_rgb(0.1, 0.1, 0.1)
         cr.fill_preserve()
-
+        
         if editorpersistance.prefs.theme == appconsts.LIGHT_THEME:
             grad = cairo.LinearGradient (0, 0, 0, h)
             for stop in BIG_TC_GRAD_STOPS:
