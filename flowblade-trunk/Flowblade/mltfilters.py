181f5a2d8d462d0ae75d8978dc4559b0bd593071
Janne Liljeblad <janne.liljeblad@gmail.com>
2018-10-02 09:28:45 +0300

    MLT version filters dropping, a better fix

--- flowblade-trunk/Flowblade/mltfilters.py
+++ flowblade-trunk/Flowblade/mltfilters.py
@@ -380,12 +380,12 @@ def load_filters_xml(services):
         filter_info = FilterInfo(f_node)
 
         if filter_info.mlt_drop_version != "":
-            if not editorstate.mlt_version_is_equal_or_greater(filter_info.mlt_drop_version):
+            if editorstate.mlt_version_is_equal_or_greater_correct(filter_info.mlt_drop_version):
                 print filter_info.name + " dropped, MLT version too high for this filter."
                 continue
 
         if filter_info.mlt_min_version != "":
-            if editorstate.mlt_version_is_equal_or_greater(filter_info.mlt_min_version):
+            if not editorstate.mlt_version_is_equal_or_greater_correct(filter_info.mlt_min_version):
                 print filter_info.name + " dropped, MLT version too low for this filter."
                 continue
 
