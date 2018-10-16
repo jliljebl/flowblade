0b83de1e4ed31176b85e19fa1203fb513c640f3e
Janne Liljeblad <janne.liljeblad@gmail.com>
2018-10-17 00:07:31 +0300

    Refix print in translations.py

--- flowblade-trunk/Flowblade/translations.py
+++ flowblade-trunk/Flowblade/translations.py
@@ -53,7 +53,7 @@ def init_languages():
             locale_path = "/usr/share/locale/"
         #  Look for installed translations in flatpak install 
         elif os.path.isfile("/app/share/flowblade/Flowblade/locale/fi/LC_MESSAGES/flowblade.mo"): # fi is the translation controlled by program author
-            print "Found translations at /usr/share/locale, using those."
+            print "Found translations at /app/share/flowblade/Flowblade/locale, using those."
             locale_path = "app/share/flowblade/Flowblade/locale"
         else:
             print "Translations at /usr/share/locale were not found, using program root directory translations."
