61e785973a770f79ce61df953c84bf024b161053
Janne Liljeblad <janne.liljeblad@gmail.com>
2019-09-01 23:50:32 +0300

    Remove encode utf-8 stuff

--- flowblade-trunk/Flowblade/editorwindow.py
+++ flowblade-trunk/Flowblade/editorwindow.py
@@ -900,14 +900,14 @@ class EditorWindow:
         sep = Gtk.SeparatorMenuItem()
         menu.append(sep)
 
-        windows_menu_item = Gtk.MenuItem(_("Window Mode").encode('utf-8'))
+        windows_menu_item = Gtk.MenuItem(_("Window Mode"))
         windows_menu =  Gtk.Menu()
         one_window = Gtk.RadioMenuItem()
-        one_window.set_label( _("Single Window").encode('utf-8'))
+        one_window.set_label(_("Single Window"))
 
         windows_menu.append(one_window)
         
-        two_windows = Gtk.RadioMenuItem.new_with_label([one_window], _("Two Windows").encode('utf-8'))
+        two_windows = Gtk.RadioMenuItem.new_with_label([one_window], _("Two Windows"))
 
         if editorpersistance.prefs.global_layout == appconsts.SINGLE_WINDOW:
             one_window.set_active(True)
@@ -921,18 +921,18 @@ class EditorWindow:
         windows_menu_item.set_submenu(windows_menu)
         menu.append(windows_menu_item)
         
-        mb_menu_item = Gtk.MenuItem(_("Middlebar Layout").encode('utf-8'))
+        mb_menu_item = Gtk.MenuItem(_("Middlebar Layout"))
         mb_menu = Gtk.Menu()
         tc_left = Gtk.RadioMenuItem()
-        tc_left.set_label(_("Timecode Left").encode('utf-8'))
+        tc_left.set_label(_("Timecode Left"))
         tc_left.connect("activate", lambda w: middlebar._show_buttons_TC_LEFT_layout(w))
         mb_menu.append(tc_left)
 
-        tc_middle = Gtk.RadioMenuItem.new_with_label([tc_left], _("Timecode Center").encode('utf-8'))
+        tc_middle = Gtk.RadioMenuItem.new_with_label([tc_left], _("Timecode Center"))
         tc_middle.connect("activate", lambda w: middlebar._show_buttons_TC_MIDDLE_layout(w))
         mb_menu.append(tc_middle)
 
-        components_centered = Gtk.RadioMenuItem.new_with_label([tc_left], _("Components Centered").encode('utf-8'))
+        components_centered = Gtk.RadioMenuItem.new_with_label([tc_left], _("Components Centered"))
         components_centered.connect("activate", lambda w: middlebar._show_buttons_COMPONETS_CENTERED_layout(w))
         mb_menu.append(components_centered)
 
@@ -946,14 +946,14 @@ class EditorWindow:
         mb_menu_item.set_submenu(mb_menu)
         menu.append(mb_menu_item)
 
-        tabs_menu_item = Gtk.MenuItem(_("Tabs Position").encode('utf-8'))
+        tabs_menu_item = Gtk.MenuItem(_("Tabs Position"))
         tabs_menu =  Gtk.Menu()
         tabs_up = Gtk.RadioMenuItem()
-        tabs_up.set_label( _("Up").encode('utf-8'))
+        tabs_up.set_label( _("Up"))
         tabs_up.connect("activate", lambda w: self._show_tabs_up(w))
         tabs_menu.append(tabs_up)
         
-        tabs_down = Gtk.RadioMenuItem.new_with_label([tabs_up], _("Down").encode('utf-8'))
+        tabs_down = Gtk.RadioMenuItem.new_with_label([tabs_up], _("Down"))
         tabs_down.connect("activate", lambda w: self._show_tabs_down(w))
 
         if editorpersistance.prefs.tabs_on_top == True:
@@ -971,24 +971,24 @@ class EditorWindow:
         sep = Gtk.SeparatorMenuItem()
         menu.append(sep)
 
-        interp_menu_item = Gtk.MenuItem(_("Monitor Playback Interpolation").encode('utf-8'))
+        interp_menu_item = Gtk.MenuItem(_("Monitor Playback Interpolation"))
         interp_menu = Gtk.Menu()
         
         interp_nearest = Gtk.RadioMenuItem()
-        interp_nearest.set_label(_("Nearest Neighbour (fast)").encode('utf-8'))
+        interp_nearest.set_label(_("Nearest Neighbour (fast)"))
         interp_nearest.connect("activate", lambda w: monitorevent.set_monitor_playback_interpolation("nearest"))
         interp_menu.append(interp_nearest)
         
-        interp_bilinear = Gtk.RadioMenuItem.new_with_label([interp_nearest], _("Bilinear (good)").encode('utf-8'))
+        interp_bilinear = Gtk.RadioMenuItem.new_with_label([interp_nearest], _("Bilinear (good)"))
         interp_bilinear.connect("activate", lambda w: monitorevent.set_monitor_playback_interpolation("bilinear"))
         interp_menu.append(interp_bilinear)
 
-        interp_bicubic = Gtk.RadioMenuItem.new_with_label([interp_nearest], _("Bicubic (better)").encode('utf-8'))
+        interp_bicubic = Gtk.RadioMenuItem.new_with_label([interp_nearest], _("Bicubic (better)"))
         interp_bicubic.set_active(True)
         interp_bicubic.connect("activate", lambda w: monitorevent.set_monitor_playback_interpolation("bicubic"))
         interp_menu.append(interp_bicubic)
 
-        interp_hyper = Gtk.RadioMenuItem.new_with_label([interp_nearest], _("Hyper/Lanczos (best)").encode('utf-8'))
+        interp_hyper = Gtk.RadioMenuItem.new_with_label([interp_nearest], _("Hyper/Lanczos (best)"))
         interp_hyper.connect("activate", lambda w: monitorevent.set_monitor_playback_interpolation("hyper"))
         interp_menu.append(interp_hyper)
 
@@ -998,13 +998,13 @@ class EditorWindow:
         sep = Gtk.SeparatorMenuItem()
         menu.append(sep)        
         
-        zoom_in_menu_item = Gtk.MenuItem(_("Zoom In").encode('utf-8'))
+        zoom_in_menu_item = Gtk.MenuItem(_("Zoom In"))
         zoom_in_menu_item.connect("activate", lambda w: updater.zoom_in())
         menu.append(zoom_in_menu_item)
-        zoom_out_menu_item = Gtk.MenuItem(_("Zoom Out").encode('utf-8'))
+        zoom_out_menu_item = Gtk.MenuItem(_("Zoom Out"))
         zoom_out_menu_item.connect("activate", lambda w: updater.zoom_out())
         menu.append(zoom_out_menu_item)
-        zoom_fit_menu_item = Gtk.MenuItem(_("Zoom Fit").encode('utf-8'))
+        zoom_fit_menu_item = Gtk.MenuItem(_("Zoom Fit"))
         zoom_fit_menu_item.connect("activate", lambda w: updater.zoom_project_length())
         menu.append(zoom_fit_menu_item)
                 
