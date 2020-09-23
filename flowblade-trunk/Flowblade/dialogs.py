05a79657c47b4783f53ecf25b3f2978b3f16d7d2
Janne Liljeblad <janne.liljeblad@gmail.com>
2020-09-23 09:36:17 +0300

    Change displayed ver strings

--- flowblade-trunk/Flowblade/dialogs.py
+++ flowblade-trunk/Flowblade/dialogs.py
@@ -521,7 +521,7 @@ def about_dialog(parent_window):
     # Application tab
     img = Gtk.Image.new_from_file(respaths.IMAGE_PATH + "flowbladeappicon.png")
     flow_label = Gtk.Label(label="Flowblade Movie Editor")
-    ver_label = Gtk.Label(label="2.6.0")
+    ver_label = Gtk.Label(label="2.6.1")
     janne_label = Gtk.Label(label="Copyright 2020 Janne Liljeblad and contributors")
     page_label = Gtk.Label(label=_("Project page:") + " " + "<a href=\"https://github.com/jliljebl/flowblade\">https://github.com/jliljebl/flowblade</a>")
     page_label.set_use_markup(True)
