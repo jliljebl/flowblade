9a25d88f7e6f2ac937657b83ff54fbcd76c6b063
Janne Liljeblad <janne.liljeblad@gmail.com>
2021-03-15 18:36:07 +0200

    script - groups

--- flowblade-trunk/Flowblade/mediaplugin.py
+++ flowblade-trunk/Flowblade/mediaplugin.py
@@ -21,8 +21,10 @@ import json
 import respaths
 
 _plugins = []
+_plugins_groups = []
 
 def init():
+    # Load Plugins
     plugins_list_json = open(respaths.MEDIA_PLUGINS_PATH + "plugins.json")
     plugins_obj = json.load(plugins_list_json)
     
@@ -32,9 +34,40 @@ def init():
         plugin = MediaPlugin(plugin_data["folder"], plugin_data["name"], plugin_data["category"])
         _plugins.append(plugin)
 
-#def get_categories_dict:
-        
-    
+    # Create categories with translated names and sorted scripts.
+    # Category names have to correspond with category names in fluxity.py.
+    _script_groups_names = {}
+    _script_groups_names["Animations"] = _("Animations")
+    _script_groups_names["Effects"] = _("Effects")
+    _script_groups_names["Cover Transitions"] = _("Cover Transitions")
+
+    load_groups = {}
+    for plugin in _plugins:
+        try:
+            translated_group_name = _script_groups_names[plugin.category]
+        except:
+            translated_group_name = "Misc"
+
+        try:
+            group = load_groups[translated_group_name]
+            group.append(plugin)
+        except:
+            load_groups[translated_group_name] = [plugin]
+
+    sorted_keys = sorted(load_groups.keys())
+    global _plugins_groups
+    for gkey in sorted_keys:
+        group = load_groups[gkey]
+        add_group = sorted(group, key=lambda plugin: plugin.name)
+        _plugins_groups.append((gkey, add_group))
+
+    """
+    for group_data in _plugins_groups:
+        group_name, group = group_data
+        print(group_name)
+        for plugin in group:
+            print(plugin.name)
+    """
 
 class MediaPlugin:
     
