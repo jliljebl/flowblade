fb6f4ae09ea03e63dc5665d08a8d21bb3bcaefa2
Janne Liljeblad <janne.liljeblad@gmail.com>
2021-11-08 14:57:50 +0200

    Add media plugins resources in setup.py package data

--- flowblade-trunk/setup.py
+++ flowblade-trunk/setup.py
@@ -37,7 +37,8 @@ flowblade_package_data = ['res/filters/*.xml','res/filters/wipes/*','res/img/*',
                           'res/proxyprofiles/*','res/darktheme/*','launch/*','res/gmic/*',
                           'res/shortcuts/*','res/css/*','res/css/assets/*','res/css/sass/*',
                           'res/css2/*','res/css2/assets/*','res/css2/sass/*',
-                          'res/css3/*','res/css3/assets/*','res/css3/sass/*']
+                          'res/css3/*','res/css3/assets/*','res/css3/sass/*',
+                          'res/mediaplugins/*']
 
 locale_files = []
 for filepath in glob.glob("Flowblade/locale/*/LC_MESSAGES/*"):
