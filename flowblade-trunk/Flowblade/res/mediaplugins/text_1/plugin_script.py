f7fbcf4cd2e54101024fd1a6bf994a97d0ff68b0
Janne Liljeblad <janne.liljeblad@gmail.com>
2022-05-19 11:40:11 +0300

    Text Plugin Work

--- flowblade-trunk/Flowblade/res/mediaplugins/text_1/plugin_script.py
+++ flowblade-trunk/Flowblade/res/mediaplugins/text_1/plugin_script.py
@@ -21,21 +21,21 @@ def init_script(fctx):
     fctx.add_editor("Line Gap", fluxity.EDITOR_INT, 8)
     fctx.add_editor("Lines Delay Frames", fluxity.EDITOR_INT_RANGE, (0, 0, 50))
     fctx.add_editor("Animation Type In", fluxity. EDITOR_OPTIONS, \
-                    (9, ["From Left Clipped", "From Right Clipped", "From Up Clipped", \
+                    (0, ["From Left Clipped", "From Right Clipped", "From Up Clipped", \
                         "From Down Clipped", "From Left", "From Right", "From Up", \
-                        "From Down", "Zoom In", "Zoom Out", "Typewriter Letter", "Typewriter Word"]))
-    fctx.add_editor("Movement In", fluxity. EDITOR_OPTIONS, (0,["Linear", "Smooth",  "Fast Start", "Slow Start", "Stepped"]))
+                        "From Down"]))
+    fctx.add_editor("Movement In", fluxity. EDITOR_OPTIONS, (1,["Linear", "Ease In", "Ease Out", "Stepped"]))
     fctx.add_editor("Frames In", fluxity.EDITOR_INT, 10)
     fctx.add_editor("Steps In", fluxity.EDITOR_INT_RANGE, (3, 2, 10))
-    fctx.add_editor("Fade In", fluxity. EDITOR_OPTIONS, (0,["Off", "Linear", "Smooth",  "Fast Start", "Slow Start", "Stepped"]))
+    fctx.add_editor("Fade In Frames", fluxity.EDITOR_INT_RANGE, (0, 0, 200))
     fctx.add_editor("Animation Type Out", fluxity. EDITOR_OPTIONS, \
-                    (3, ["From Left Clipped", "From Right Clipped", "From Up Clipped", \
+                    (0, ["From Left Clipped", "From Right Clipped", "From Up Clipped", \
                         "From Down Clipped", "From Left", "From Right", "From Up", \
-                        "From Down", "Zoom In", "Zoom Out", "Typewriter Letter", "Typewriter Word"]))
-    fctx.add_editor("Movement Out", fluxity. EDITOR_OPTIONS, (0,["Linear", "Smooth",  "Fast Start", "Slow Start", "Stepped"]))
+                        "From Down"]))
+    fctx.add_editor("Movement Out", fluxity. EDITOR_OPTIONS, (0,["Linear", "Ease In", "Ease Out", "Stepped"]))
     fctx.add_editor("Frames Out", fluxity.EDITOR_INT, 10)
     fctx.add_editor("Steps Out", fluxity.EDITOR_INT_RANGE, (3, 2, 10))
-    fctx.add_editor("Fade Out", fluxity. EDITOR_OPTIONS, (0, ["Off", "Linear", "Smooth",  "Fast Start", "Slow Start", "Stepped"]))
+    fctx.add_editor("Fade Out Frames", fluxity.EDITOR_INT_RANGE, (0, 0, 200))
     fctx.add_editor("Background", fluxity. EDITOR_OPTIONS, (1, ["No Backround", "Color Background"]))
     fctx.add_editor("Background Pad", fluxity.EDITOR_INT, 30)
 
@@ -56,14 +56,14 @@ def init_render(fctx):
     animation_in_type = fctx.get_editor_value("Animation Type In")
     movement_in_type = fctx.get_editor_value("Movement In")
     steps_in = fctx.get_editor_value("Steps In")
-    fade_in_type = fctx.get_editor_value("Fade In")
-    in_anim_data = (animation_in_type, movement_in_type, steps_in, fade_in_type)
+    fade_in_frames = fctx.get_editor_value("Fade In Frames")
+    in_anim_data = (animation_in_type, movement_in_type, steps_in, fade_in_frames )
 
     animation_out_type = fctx.get_editor_value("Animation Type Out")
     movement_out_type = fctx.get_editor_value("Movement Out")
     steps_out = fctx.get_editor_value("Steps Out")
-    fade_out_type = fctx.get_editor_value("Fade Out")
-    out_anim_data = (animation_out_type, movement_out_type, steps_out, fade_out_type)
+    fade_out_frames = fctx.get_editor_value("Fade Out Frames")
+    out_anim_data = (animation_out_type, movement_out_type, steps_out, fade_out_frames)
     
     # Create linetext objects
     lines = text.splitlines()
@@ -131,10 +131,9 @@ class LineText:
     ZOOM_OUT = 9
     
     LINEAR = 0
-    SMOOTH = 1
-    FAST_START = 2
-    SLOW_START = 3
-    STEPPED = 4
+    EASE_IN = 1
+    EASE_OUT = 2
+    STEPPED = 3
 
     ANIMATION_IN = 0
     ANIMATION_OUT = 1
@@ -152,8 +151,8 @@ class LineText:
         self.font_data = font_data
         self.pos = pos
         self.line_index, self.line_gap, self.line_delay = line_info
-        self.animation_type_in, self.movement_type_in, self.steps_in, self.fadein_type = animation_in_data
-        self.animation_type_out, self.movement_type_out, self.steps_out, self.fadeout_type = animation_out_data
+        self.animation_type_in, self.movement_type_in, self.steps_in, self.fade_in_frames = animation_in_data
+        self.animation_type_out, self.movement_type_out, self.steps_out, self.fade_out_frames = animation_out_data
         self.in_frames = in_frames
         self.out_frames = out_frames
         self.affine = None
@@ -165,8 +164,7 @@ class LineText:
         self.opacity = fluxity.AnimatedValue()
         
         # Animation In
-        start_x, start_y, end_x, end_y, start_scale, end_scale, \
-        anchor_x, anchor_y = self._get_in_animation_affine_data(fctx, bg_area)
+        start_x, start_y, end_x, end_y, start_scale, end_scale = self._get_in_animation_affine_data(fctx, bg_area)
  
         frame_start = 0
         length = self.in_frames
@@ -174,14 +172,10 @@ class LineText:
         animation_type = self.animation_type_in
         self._apply_affine_data_with_movement(movement_type, animation_type, start_x, \
                                               start_y, end_x, end_y, start_scale, \
-                                              end_scale, anchor_x, anchor_y, \
-                                              frame_start, length, self.steps_in)
-                                     
-        self._apply_fade(self.fadein_type, frame_start, frame_start + length, LineText.ANIMATION_IN)
+                                              end_scale, frame_start, length, self.steps_in)
 
         # Animation Out
-        start_x, start_y, end_x, end_y, start_scale, end_scale, \
-        anchor_x, anchor_y = self._get_out_animation_affine_data(fctx, bg_area)
+        start_x, start_y, end_x, end_y, start_scale, end_scale = self._get_out_animation_affine_data(fctx, bg_area)
         
         frame_start = fctx.get_length() - self.out_frames - 1
         length = self.out_frames
@@ -190,57 +184,32 @@ class LineText:
         animation_type = self.animation_type_out
         self._apply_affine_data_with_movement(movement_type, animation_type, start_x, \
                                               start_y, end_x, end_y, start_scale, \
-                                              end_scale, anchor_x, anchor_y, \
-                                              frame_start, length, self.steps_out)
-                                     
-        self._apply_fade(self.fadeout_type, frame_start, frame_start + length, LineText.ANIMATION_OUT)
+                                              end_scale, frame_start, length, self.steps_out)
+        fctx.log_line(str(self.affine.x.keyframes))                 
+        self._apply_fade(fctx)
 
     def _apply_affine_data_with_movement(self, movement_type, animation_type, start_x, start_y, end_x, end_y, \
-                                         start_scale, end_scale, anchor_x, anchor_y, \
-                                         frame_start, frame_end, steps):
+                                         start_scale, end_scale, frame_start, frame_end, steps):
                                          
         if movement_type == LineText.LINEAR:
             self._apply_linear_movement(self.affine.x, start_x, end_x, frame_start, frame_end)
             self._apply_linear_movement(self.affine.y, start_y, end_y, frame_start, frame_end)
             self._apply_linear_movement(self.affine.scale_x, start_scale, end_scale, frame_start, frame_end)
-            self._apply_linear_movement(self.affine.scale_y, start_scale, end_scale, frame_start, frame_end)
-            self._apply_no_movement(self.affine.anchor_x, anchor_x)
-            self._apply_no_movement(self.affine.anchor_y, anchor_y)
-        elif movement_type == LineText.SMOOTH:
-            self._apply_smooth_movement(self.affine.x, start_x, end_x, frame_start, frame_end)
-            self._apply_smooth_movement(self.affine.y, start_y, end_y, frame_start, frame_end)
-            self._apply_linear_movement(self.affine.scale_x, start_scale, end_scale, frame_start, frame_end)
-            self._apply_linear_movement(self.affine.scale_y, start_scale, end_scale, frame_start, frame_end)
-            self._apply_no_movement(self.affine.anchor_x, anchor_x)
-            self._apply_no_movement(self.affine.anchor_y, anchor_y)
-        elif movement_type == LineText.FAST_START:
+            self._apply_linear_movement(self.affine.scale_y, start_scale, end_scale, frame_start, frame_end) 
+        elif movement_type == LineText.EASE_IN:
             if animation_type in LineText.HORIZONTAL_ANIMATIONS:
                 self._apply_fast_start_movement(self.affine.x, start_x, end_x, frame_start, frame_end)
                 self._apply_no_movement(self.affine.y, start_y)
             elif animation_type in LineText.VERTICAL_ANIMATIONS:
                 self._apply_no_movement(self.affine.x, start_x)
                 self._apply_fast_start_movement(self.affine.y, start_y, end_y, frame_start, frame_end)
-            else: # Zooms
-                self._apply_no_movement(self.affine.y, start_y)
-                self._apply_no_movement(self.affine.x, start_x)
-                self._apply_fast_start_movement(self.affine.scale_x, start_scale, end_scale, frame_start, frame_end)
-                self._apply_fast_start_movement(self.affine.scale_y, start_scale, end_scale, frame_start, frame_end)
-                self._apply_no_movement(self.affine.anchor_x, anchor_x)
-                self._apply_no_movement(self.affine.anchor_y, anchor_y)
-        elif movement_type == LineText.SLOW_START:
+        elif movement_type == LineText.EASE_OUT:
             if animation_type in LineText.HORIZONTAL_ANIMATIONS:
                 self._apply_slow_start_movement(self.affine.x, start_x, end_x, frame_start, frame_end)
                 self._apply_no_movement(self.affine.y, start_y)
             elif animation_type in LineText.VERTICAL_ANIMATIONS:
                 self._apply_no_movement(self.affine.x, start_x)
                 self._apply_slow_start_movement(self.affine.y, start_y, end_y, frame_start, frame_end)
-            else: # Zooms
-                self._apply_no_movement(self.affine.y, start_y)
-                self._apply_no_movement(self.affine.x, start_x)
-                self._apply_slow_start_movement(self.affine.scale_x, start_scale, end_scale, frame_start, frame_end)
-                self._apply_slow_start_movement(self.affine.scale_y, start_scale, end_scale, frame_start, frame_end)
-                self._apply_no_movement(self.affine.anchor_x, anchor_x)
-                self._apply_no_movement(self.affine.anchor_y, anchor_y)
         elif movement_type == LineText.STEPPED:
             if animation_type in LineText.HORIZONTAL_ANIMATIONS:
                 self._apply_stepped_movement(self.affine.x, start_x, end_x, frame_start, frame_end, steps)
@@ -248,13 +217,6 @@ class LineText:
             elif animation_type in LineText.VERTICAL_ANIMATIONS:
                 self._apply_no_movement(self.affine.x, start_x)
                 self._apply_stepped_movement(self.affine.y, start_y, end_y, frame_start, frame_end, steps)
-            else: # Zooms
-                self._apply_no_movement(self.affine.y, start_y)
-                self._apply_no_movement(self.affine.x, start_x)
-                self._apply_stepped_movement(self.affine.scale_x, start_scale, end_scale, frame_start, frame_end, steps)
-                self._apply_stepped_movement(self.affine.scale_y, start_scale, end_scale, frame_start, frame_end, steps)
-                self._apply_no_movement(self.affine.anchor_x, anchor_x)
-                self._apply_no_movement(self.affine.anchor_y, anchor_y)
 
     def _get_in_animation_affine_data(self, fctx, bg_area):
         static_x, static_y = self.pos
@@ -278,8 +240,6 @@ class LineText:
         
         start_scale = 1.0
         end_scale = 1.0
-        anchor_x = 0.0
-        anchor_y = 0.0
         
         if self.animation_type_in == LineText.FROM_LEFT_CLIPPED:
             start_x = static_x - lw
@@ -321,28 +281,8 @@ class LineText:
             start_y = screen_h
             end_x = static_x + pad
             end_y = static_y + pad
-        elif self.animation_type_in == LineText.ZOOM_IN:
-            start_scale = 0.5
-            end_scale = 1.0
-            anchor_x = lw / 2.0
-            anchor_y = lh / 2.0
-            start_x = static_x + pad
-            start_y = static_y + pad
-            end_x = static_x + pad
-            end_y = static_y + pad
-        elif self.animation_type_in == LineText.ZOOM_OUT:
-            start_scale = 2.0
-            end_scale = 1.0
-            anchor_x = lw / 2.0
-            anchor_y = lh / 2.0
-            start_x = static_x + pad
-            start_y = static_y + pad
-            end_x = static_x + pad
-            end_y = static_y + pad
 
-            start_x, start_y, end_x, end_y, start_scale, end_scale, anchor_x, anchor_y
-
-        return (start_x, start_y, end_x, end_y, start_scale, end_scale, anchor_x, anchor_y)
+        return (start_x, start_y, end_x, end_y, start_scale, end_scale)
 
     def _get_out_animation_affine_data(self, fctx, bg_area):
         static_x, static_y = self.pos
@@ -358,6 +298,7 @@ class LineText:
             lw = bg_width
 
         # Get line y position
+        first_line_static_y = static_y
         for line_index in range(self.line_index):
             static_y = static_y + lh + self.line_gap
 
@@ -366,8 +307,6 @@ class LineText:
         
         start_scale = 1.0
         end_scale = 1.0
-        anchor_x = 0.0
-        anchor_y = 0.0
         
         if self.animation_type_out == LineText.FROM_LEFT_CLIPPED:
             start_x = static_x + pad
@@ -390,67 +329,35 @@ class LineText:
             end_x = static_x + pad 
             end_y = static_y + lh + pad * 2
         elif self.animation_type_out == LineText.FROM_LEFT:
-            start_x = static_x
-            start_y = static_y
+            start_x = static_x + pad
+            start_y = static_y + pad
             end_x = -lw
-            end_y = static_y
+            end_y = static_y + pad
         elif self.animation_type_out == LineText.FROM_RIGHT:
-            start_x = static_x
-            start_y = static_y
+            start_x = static_x + pad
+            start_y = static_y + pad
             end_x = screen_w + lw
-            end_y = static_y
+            end_y = static_y + pad
         elif self.animation_type_out == LineText.FROM_UP:
-            start_x = static_x
-            start_y = static_y
-            end_x = static_x
-            end_y = -lh
-        elif self.animation_type_out == LineText.ZOOM_IN:
-            start_scale = 1.0
-            end_scale = 0.5
-            anchor_x = lw / 2.0
-            anchor_y = lh / 2.0
-            start_x = static_x
-            start_y = static_y
-            end_x = static_x
-            end_y = static_y
-        elif self.animation_type_out == LineText.ZOOM_OUT:
-            start_scale = 1.0
-            end_scale = 2.0
-            anchor_x = lw / 2.0
-            anchor_y = lh / 2.0
-            start_x = static_x
-            start_y = static_y
-            end_x = static_x
-            end_y = static_y
+            start_x = static_x + pad
+            start_y = static_y + pad
+            end_x = static_x + pad
+            end_y = static_y - first_line_static_y - first_line_static_y + lh
+        elif self.animation_type_out == LineText.FROM_DOWN:
+            start_x = static_x + pad
+            start_y = static_y + pad
+            end_x = static_x + pad
+            end_y = screen_h + static_y - first_line_static_y
 
-        return (start_x, start_y, end_x, end_y, start_scale, end_scale, anchor_x, anchor_y)
+        return (start_x, start_y, end_x, end_y, start_scale, end_scale)
         
-    def _apply_fade(self, fade_type, frame_start, frame_end, direction):
-        fade_type = self.fadein_type
-        if fade_type == 0:
-            self._apply_no_movement(self.opacity, 1.0)
-            return
-        else:
-            fade_type = fade_type - 1
+    def _apply_fade(self, fctx):
+        self.opacity.add_keyframe_at_frame(0, 0.0, fluxity.KEYFRAME_LINEAR)
+        self.opacity.add_keyframe_at_frame(self.fade_in_frames, 1.0, fluxity.KEYFRAME_LINEAR) # With self.fade_in_frames == 0 this replaces kf from line above.
+        if self.fade_out_frames > 0:
+            self.opacity.add_keyframe_at_frame(fctx.get_length() - self.fade_out_frames - 1, 1.0, fluxity.KEYFRAME_LINEAR)
+            self.opacity.add_keyframe_at_frame(fctx.get_length() - 1, 0.0, fluxity.KEYFRAME_LINEAR)
 
-        if direction == LineText.ANIMATION_IN:
-            opa1 = 0.0
-            opa2 = 1.0
-        else:
-            opa1 = 1.0
-            opa2 = 0.0
-
-        if fade_type == LineText.LINEAR:
-            self._apply_linear_movement(self.opacity, opa1, opa2, frame_start, frame_end)
-        elif fade_type == LineText.SMOOTH:
-            self._apply_smooth_movement(self.opacity, opa1, opa2, frame_start, frame_end)
-        elif fade_type == LineText.FAST_START:
-            self._apply_fast_start_movement(self.opacity, opa1, opa2, frame_start, frame_end)
-        elif fade_type == LineText.SLOW_START:
-            self._apply_slow_start_movement(self.opacity, opa1, opa2, frame_start, frame_end)
-        elif fade_type == LineText.STEPPED:
-            self._apply_stepped_movement(self.opacity, opa1, opa2,  frame_start, frame_end)
-            
     def _apply_no_movement(self, animated_value, value):
         animated_value.add_keyframe_at_frame(0, value, fluxity.KEYFRAME_LINEAR)
            
@@ -460,21 +367,21 @@ class LineText:
 
     def _apply_smooth_movement(self, animated_value, start_val, end_val, start_frame, length):
         animated_value.add_keyframe_at_frame(start_frame, start_val, fluxity.KEYFRAME_SMOOTH)   
-        animated_value.add_keyframe_at_frame(start_frame + length, end_val, fluxity.KEYFRAME_SMOOTH)
+        animated_value.add_keyframe_at_frame(start_frame + length, end_val, fluxity.KEYFRAME_LINEAR)
 
     def _apply_fast_start_movement(self, animated_value, start_val, end_val, start_frame, length):
         mid_kf_frame = int(length * 0.2)
         mid_kf_value = start_val + (end_val - start_val) * 0.8
         animated_value.add_keyframe_at_frame(start_frame, start_val, fluxity.KEYFRAME_SMOOTH)
-        animated_value.add_keyframe_at_frame(mid_kf_frame, mid_kf_value, fluxity.KEYFRAME_SMOOTH)
-        animated_value.add_keyframe_at_frame(start_frame + length, end_val, fluxity.KEYFRAME_SMOOTH)
+        animated_value.add_keyframe_at_frame(start_frame + mid_kf_frame, mid_kf_value, fluxity.KEYFRAME_SMOOTH)
+        animated_value.add_keyframe_at_frame(start_frame + length, end_val, fluxity.KEYFRAME_LINEAR)
 
     def _apply_slow_start_movement(self, animated_value, start_val, end_val, start_frame, length):
         mid_kf_frame = int(length * 0.8)
         mid_kf_value = start_val + (end_val - start_val) * 0.2
         animated_value.add_keyframe_at_frame(start_frame, start_val, fluxity.KEYFRAME_SMOOTH)
-        animated_value.add_keyframe_at_frame(mid_kf_frame, mid_kf_value, fluxity.KEYFRAME_SMOOTH)
-        animated_value.add_keyframe_at_frame(start_frame + length, end_val, fluxity.KEYFRAME_SMOOTH)
+        animated_value.add_keyframe_at_frame(start_frame + mid_kf_frame, mid_kf_value, fluxity.KEYFRAME_SMOOTH)
+        animated_value.add_keyframe_at_frame(start_frame + length, end_val, fluxity.KEYFRAME_LINEAR)
 
     def _apply_stepped_movement(self, animated_value, start_val, end_val, start_frame, length, steps):
         step_frames = int(round(length/steps))
