823dbaca0a67dd816d76c310c9119ea07c0ef5de
Janne Liljeblad <janne.liljeblad@gmail.com>
2023-01-21 18:23:45 +0200

    Make mltheadlessutils.py use mltinit.py

--- flowblade-trunk/Flowblade/tools/mltheadlessutils.py
+++ flowblade-trunk/Flowblade/tools/mltheadlessutils.py
@@ -33,10 +33,7 @@ import os
 import ccrutils
 import editorstate
 import editorpersistance
-import mltfilters
-import mltenv
-import mltprofiles
-import mlttransitions
+import mltinit
 import processutils
 import renderconsumer
 import respaths
@@ -58,27 +55,7 @@ def mlt_env_init(root_path, session_id):
     userfolders.init()
     editorpersistance.load()
 
-    # Init translations module with translations data
-    translations.init_languages()
-    translations.load_filters_translations()
-    mlttransitions.init_module()
-
-    repo = mlt.Factory().init()
-    processutils.prepare_mlt_repo(repo)
-    
-    # Set numeric locale to use "." as radix, MLT initilizes this to OS locale and this causes bugs 
-    locale.setlocale(locale.LC_NUMERIC, 'C')
-
-    # Check for codecs and formats on the system
-    mltenv.check_available_features(repo)
-    renderconsumer.load_render_profiles()
-
-    # Load filter and compositor descriptions from xml files.
-    mltfilters.load_filters_xml(mltenv.services)
-    mlttransitions.load_compositors_xml(mltenv.transitions)
-
-    # Create list of available mlt profiles
-    mltprofiles.load_profile_list()
+    mltinit.init_with_translations()
     
     ccrutils.init_session_folders(session_id)
     
