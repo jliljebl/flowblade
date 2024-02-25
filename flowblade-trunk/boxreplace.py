1d669c0a55224d538e1a84d3553b129a5eb3ffeb
Janne Liljeblad <janne.liljeblad@gmail.com>
2024-02-25 18:09:24 +0200

    Stex, fix renderqueueview vbox

--- flowblade-trunk/boxreplace.py
+++ flowblade-trunk/boxreplace.py
@@ -48,12 +48,13 @@ def _substring_replace(sub_string, replace_string):
     print("Changed lines for ", replace_string, line_count)
 
 
-def _append_line_after(starts_string, sub_string, append_line):
+def _insert_line_after(starts_string, sub_string, append_line, lines_in_between=0, show_files=False):
     files_changed_count = 0
-   
+
+    changed_files = []
     for root, dirnames, filenames in os.walk(src_dir):
         for filename in filenames:
-            
+            insert_index = -99
             if filename.endswith(".py") == False:
                 continue
 
@@ -71,26 +72,35 @@ def _append_line_after(starts_string, sub_string, append_line):
                 new_lines.append(line)
                 if line.startswith(starts_string) and changed == False:
                     if sub_string in line:
-                        new_lines.append(append_line + "\n")
-                        changed = True
-
+                        if lines_in_between == 0:
+                            new_lines.append(append_line + "\n")
+                            changed = True
+                        else:
+                            insert_index = len(new_lines) + lines_in_between
+                if len(new_lines) == insert_index:
+                    new_lines.append(append_line + "\n")
+                    changed = True
+                    
             if changed == True:
                 files_changed_count += 1
+                changed_files.append(filename)
                 with open(file_path, "w") as f:
                     f.writelines(new_lines)  
    
     print("Files changed with added line: " + append_line + " " + str(files_changed_count))
-
+    if show_files == True:
+        print(changed_files)
+    
 
 _substring_replace(' Gtk.VBox', ' gtkbox.VBox')
 _substring_replace(' Gtk.HBox', ' gtkbox.HBox')
 
-_append_line_after("from gi.repository import", "Gtk", "import gtkbox")
+_insert_line_after("from gi.repository import", "Gtk", "import gtkbox")
 
 _substring_replace("gi.require_version('Gtk', '3.0')","gi.require_version('Gtk', '4.0')")
 
 
 _substring_replace('class RenderQueueView(Gtk.VBox):', 'class RenderQueueView(Gtk.Box):')
-
+_insert_line_after('class RenderQueueView(Gtk.Box):', 'Gtk.Box', '        gtkbox.set_default_vertical(self)', 5, True)
 
 
