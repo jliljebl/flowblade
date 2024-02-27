91be816419ef7d8d21cbcb7f5a4d4a1c8316475e
Janne Liljeblad <janne.liljeblad@gmail.com>
2024-02-27 11:07:10 +0200

    Step comment out add events

--- flowblade-trunk/boxreplace.py
+++ flowblade-trunk/boxreplace.py
@@ -180,3 +180,4 @@ _insert_line_after('class TimeLineScroller(Gtk.Scrollbar):', 'Gtk.Scrollbar', ' 
 
 _comment_out_with_substring("show_all", False)
 _comment_out_with_substring("dnd.", False)
+_comment_out_with_substring("add_events", False)
