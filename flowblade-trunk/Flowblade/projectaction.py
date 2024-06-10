ca5ba3a4df06bd03cedf4c1d0ca58ffc32c775d5
Janne Liljeblad <janne.liljeblad@gmail.com>
2024-06-10 14:01:46 +0300

    Fix media file popovers

--- flowblade-trunk/Flowblade/projectaction.py
+++ flowblade-trunk/Flowblade/projectaction.py
@@ -2242,7 +2242,9 @@ def media_file_popover_mouse_right_pressed(widget, media_file, event):
     guipopover.media_file_popover_show(media_file, widget, event.x, event.y, media_file_menu_item_selected, media_file_menu_rating_item_selected)
     
 def media_file_menu_item_selected(action, variant, data):
-    item_id, media_file = data
+    item_id, unused = data
+    global _popover_media_file
+    media_file = _popover_media_file
     if item_id == "File Properties":
         _display_file_info(media_file)
     if item_id == "Open in Clip Monitor":
