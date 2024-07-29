46f3fbe17e25a04bce16d2a1b972670ddee76e28
Janne Liljeblad <janne.liljeblad@gmail.com>
2024-07-29 09:43:57 +0300

    Fix mouse scrolling for GeometryNoKeyframes editor

--- flowblade-trunk/Flowblade/keyframeeditor.py
+++ flowblade-trunk/Flowblade/keyframeeditor.py
@@ -2245,6 +2245,20 @@ class GeometryNoKeyframes(Gtk.VBox):
     def update_clip_pos(self):
         pass
 
+    def mouse_scroll_up(self):
+        view_size_index = self.geom_buttons_row.size_select.get_active()
+        view_size_index = view_size_index - 1
+        if view_size_index < 0:
+            view_size_index = 0
+        self.geom_buttons_row.size_select.set_active(view_size_index)
+
+    def mouse_scroll_down(self):
+        view_size_index = self.geom_buttons_row.size_select.get_active()
+        view_size_index = view_size_index + 1
+        if view_size_index > 2:
+            view_size_index = 2
+        self.geom_buttons_row.size_select.set_active(view_size_index)
+
 
 class FilterRotatingGeometryEditor(FilterRectGeometryEditor):
 
