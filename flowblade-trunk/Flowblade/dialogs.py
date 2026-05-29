54b68c4532e4d50e3f617a38a680315eb159f11e
Janne Liljeblad <janne.liljeblad@gmail.com>
2026-05-29 09:30:41 +0300

    Release strings for 2.24.2

--- flowblade-trunk/Flowblade/dialogs.py
+++ flowblade-trunk/Flowblade/dialogs.py
@@ -598,7 +598,7 @@ def about_dialog(parent_window):
     # Application tab
     img = Gtk.Image.new_from_file(respaths.IMAGE_PATH + "flowbladeappicon.png")
     flow_label = Gtk.Label(label="Flowblade Movie Editor")
-    ver_label = Gtk.Label(label="2.24.1")
+    ver_label = Gtk.Label(label="2.24.2")
     janne_label = Gtk.Label(label="Copyright 2026 Janne Liljeblad and contributors.")
     page_label = Gtk.Label(label=_("Project page:") + " " + "<a href=\"https://github.com/jliljebl/flowblade\">https://github.com/jliljebl/flowblade</a>")
     page_label.set_use_markup(True)
